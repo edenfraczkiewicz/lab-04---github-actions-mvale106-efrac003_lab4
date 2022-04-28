07474d24c636e8fc0b471e88cdca15d8fa134fba
mvalencia4578 <marissa.valencia4578@gmail.com>
2022-04-28 14:07:03 -0700

    Added constructor

--- rectangle.cpp
+++ rectangle.cpp
@@ -12,6 +12,11 @@ int Rectangle::area() {
 	return this->width * this->height;
 }
 
+Rectangle(int w, int h) {
+	this->width = w;
+	this->height = h;
+}
+
 int Rectangle::perimeter() {
 	return (this->width *2) + (this->height*2);
 }  
